49e404d00f36132d08adae371b374d6e47903d65
vamazur <71633738+vamazur@users.noreply.github.com>
2020-11-04 13:58:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
+
+let mainWindow;
+
+app.setPath("userData", __dirname + "/saved_recordings")
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
